4594f6a813ec66a252816b8b66e1fa2a836de252
Limseunga <limsa50@naver.com>
2025-03-28 17:18:22 +0900

    week4

---
+++ week4/read_words.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef GUARD_read_words
+#define GUARD_read_words
+
+// read_words.h
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::istream& read_words(std::istream&, std::vector<std::string>&);
+
+#endif
